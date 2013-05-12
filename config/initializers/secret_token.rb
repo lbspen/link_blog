7adb4351d6d96c371d15266ddc978a92ab881721
lbs <lbspen@gmail.com>
2013-05-12 00:07:20 -0700

    Initial generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkBlog::Application.config.secret_token = '1da2f3bb87bad0f61d87379bd94b71a199d395fc778620f3da8456164067f48a37a7cc70e10f09ba30118ad97f7a22eaf2c45b4ab57c3e8733d447221caa8448'
