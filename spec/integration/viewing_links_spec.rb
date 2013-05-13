99317884d2ab6de286c0bcbc5d45e890864f1ee7
lbs <lbspen@gmail.com>
2013-05-13 14:42:48 -0700

    renamed file

--- spec/integration/viewing_links_spec.rb
+++ spec/integration/viewing_links_spec.rb
@@ -1,10 +1,22 @@
 require 'spec_helper'
 
 feature "Viewing links" do
-	scenario "Listing all projects" do
+	scenario "Listing all links" do
 		link = Factory.create(:link, :URL => 'www.evernote.com')
 		visit '/'
 		click_link 'www.evernote.com'
 		page.current_url.should == link_url(link)
+		link = Factory.create(:link, :URL => 'www.reddit.com')
+		visit '/'
+		click_link 'www.reddit.com'
+		page.current_url.should == link_url(link)
+		link = Factory.create(:link, :URL => 'www.facebook.com')
+		visit '/'
+		click_link 'www.facebook.com'
+		page.current_url.should == link_url(link)
+		visit '/'
+		page.body.should match /facebook.*reddit.*evernote/m
+
+
 	end
-end
+end 
